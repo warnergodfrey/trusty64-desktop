05ec4f4b2f9490a3299b8dfd5330a1191da30924
Warner Godfrey <warner@godfrey.io>
2015-08-08 23:03:12 +1000

    provision desktop

---
+++ provision.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -e
+
+apt-get update
+
+apt-get install -y --no-install-recommends ubuntu-desktop
+apt-get install -y git
+apt-get install -y docker
+
--- vagrant_sudo.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash -e
-
-echo 'vagrant ALL=NOPASSWD:ALL' > /etc/sudoers.d/vagrant
-chmod 0440 /etc/sudoers.d/vagrant
