403f3ba232ee59d72036e5820b876e05550c95d9
Warner Godfrey <w@godfrey.io>
2015-08-13 16:17:21 +1000

    Update provision.sh
    fixed docker install

--- provision.sh
+++ provision.sh
@@ -9,7 +9,7 @@ apt-get install -y unity xterm gdm menu gksu synaptic acpi-support gcc gnome-ter
 apt-get install -y ubuntu-desktop --no-install-recommends
 apt-get install -y unity-tweak-tool
 apt-get install -y zip
-apt-get install -y docker
+apt-get install -y docker.io
 apt-get install -y git
 apt-get install -y vim-gnome
 apt-get install -y ack-grep
@@ -17,5 +17,7 @@ apt-get install -y python
 apt-get install -y python-pip
 apt-get install -y rake
 apt-get install -y google-chrome-stable
+
+usermod -aG docker vagrant
 
 pip install awscli
